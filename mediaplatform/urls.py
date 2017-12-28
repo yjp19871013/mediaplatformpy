f0682ccb35d564e25feddbb652c16a9137b1ab91
yjp <yjp19871013@126.com>
2017-12-28 14:45:44 +0800

    Rename path.

--- mediaplatform/urls.py
+++ mediaplatform/urls.py
@@ -5,6 +5,6 @@ app_name = 'mediaplatform'
 urlpatterns = [
     path('', views.home, name='home'),
     path('user_info/', views.user_info, name='user_info'),
-    path('contacts_update/', views.contacts_update, name='contacts_update'),
-    re_path('contacts_user_details/(?P<user_id>[0-9]+)/', views.contacts_user_details, name='contacts_user_details'),
+    path('api_contacts_update/', views.contacts_update, name='contacts_update'),
+    re_path('api_contacts_user_details/(?P<user_id>[0-9]+)/', views.contacts_user_details, name='contacts_user_details'),
 ]
