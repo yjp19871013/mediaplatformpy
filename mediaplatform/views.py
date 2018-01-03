9009f889fc9bc3d4dbdd9a5f8bfaec906cd3ad5f
yjp <yjp19871013@126.com>
2018-01-03 15:27:46 +0800

    Change api.

--- mediaplatform/views.py
+++ mediaplatform/views.py
@@ -1,10 +1,10 @@
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.decorators.cache import never_cache
 from django.views.decorators.http import require_GET
-from rest_framework import status
 from rest_framework.decorators import api_view
-from rest_framework.response import Response
+from rest_framework.utils import json
 
 from mediaplatform.models import Contacts
 from mediaplatform.serializers import ContactsSerializer
@@ -38,18 +38,23 @@ def user_info(request):
 def api_contacts_user_details(request, user_id):
     contacts = Contacts.objects.filter(user_id=user_id)
     if contacts.count() == 0:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+        ret_dict = {'data': '', 'error': '无此用户'}
+        return HttpResponse(json.dumps(ret_dict), content_type="application/json")
     else:
         serializer = ContactsSerializer(contacts, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        ret_dict = {'data': serializer.data, 'error': ''}
+        return HttpResponse(json.dumps(ret_dict), content_type="application/json")
 
 
 @api_view(['POST'])
 def api_contacts_update(request):
     serializer = ContactsSerializer(data=request.data, many=True)
     if serializer.is_valid():
+        Contacts.objects.all().delete()
         serializer.save()
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        ret_dict = {'error': ''}
+        return HttpResponse(json.dumps(ret_dict), content_type="application/json")
     else:
-        return Response(serializer.errors)
+        ret_dict = {'error': serializer.errors}
+        return HttpResponse(json.dumps(ret_dict), content_type="application/json")
 
